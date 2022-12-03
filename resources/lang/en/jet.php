bbe534289bc0453d1584ca46f558a580bc76d993
ArtMin96 <artminasyanart96@gmail.com>
2022-12-04 01:51:47 +0400

    Translation refactoring

--- resources/lang/en/jet.php
+++ resources/lang/en/jet.php
@@ -13,20 +13,6 @@ return [
         'current_password' => 'Current password',
     ],
 
-    'fields' => [
-        'email' => 'Email',
-        'login' => 'Login',
-        'name' => 'Name',
-        'password' => 'Password',
-        'password_confirm' => 'Password confirm',
-        'new_password' => 'New password',
-        'new_password_confirmation' => 'Confirm password',
-        'token_name' => 'Token name',
-        'abilities' => 'Abilities',
-        '2fa_code' => 'Code',
-        '2fa_recovery_code' => 'Recovery Code',
-    ],
-
     'permissions' => [
         'owner' => 'Owner',
         'admin' => [
